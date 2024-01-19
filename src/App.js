26c7878f1c7643ddacc0cce47d870f95d8d0c8d1
Itumeleng Kekana <itumeleng@letswhoosh.com>
2024-01-19 14:22:07 +0200

    added dom sanitizer, initial markdown, and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
+import React from "react";
 import "./App.css";
 
+import { marked } from "marked";
+import { initialMarkdown } from "./lib";
+import * as DOMPurify from "dompurify";
+
 function App() {
-	return <div className="App"></div>;
+	const [markdown, setMarkdown] = React.useState("");
+
+	const updateMarkdown = (markdownValue) => {
+		setMarkdown(markdownValue);
+	};
+
+	React.useEffect(() => {
+		updateMarkdown(initialMarkdown);
+	}, []);
+
+	marked(markdown);
+
+	return (
+		<div className="App">
+			<h1 className="app-header">Markdown Previewer</h1>
+			<div className="container">
+				<div className="editor">
+					<div className="heading">
+						<h4>Editor</h4>
+					</div>
+					<textarea
+						name=""
+						id="editor"
+						value={markdown}
+						onChange={(e) => {
+							updateMarkdown(e.target.value);
+						}}
+					></textarea>
+				</div>
+				<div className="preview">
+					<div className="heading">
+						<h4>Markdown Preview</h4>
+					</div>
+					<div
+						id="preview"
+						dangerouslySetInnerHTML={{
+							__html: DOMPurify.sanitize(marked(markdown)),
+						}}
+					></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
